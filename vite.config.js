01f9144620cba4b8c781802eab9655ff52b3fd5d
Akashkadam131724 <kadamakash131724@gmail.com>
2023-12-05 12:48:35 +0530

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Akashkadam131724/mern-portfolio-fron-end",
+  base: "/mern-portfolio-fron-end",
   plugins: [react()],
 });
